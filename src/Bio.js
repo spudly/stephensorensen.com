494992d2ba1c7bc099e5d9f1dde35781a9b53f46
Stephen Sorensen <shuoink@gmail.com>
2017-01-24 19:28:43 +0000

    make <Social /> part of <Bio />

--- src/Bio.js
+++ src/Bio.js
@@ -1,6 +1,5 @@
 import Gravatar from 'react-gravatar';
 import React from 'react';
-import Social from './Social';
 
 const Bio = () =>
   <div className="h-card">
@@ -23,7 +22,18 @@ const Bio = () =>
 
     <p>No, I&apos;m not looking for a job, but thanks for asking.</p>
 
-    <Social />
+    <h2>You can find me on...</h2>
+
+    <ul>
+      <li><a rel="me" href="https://www.facebook.com/stephenjohnsorensen">Facebook</a></li>
+      <li><a rel="me" href="https://twitter.com/shuoink">Twitter</a></li>
+      <li><a rel="me" href="https://plus.google.com/+StephenSorensen">Google+</a></li>
+      <li><a rel="me" href="https://medium.com/@shuoink">Medium</a></li>
+      <li><a rel="me" href="https://github.com/spudly">Github</a></li>
+      <li><a rel="me" href="http://stackoverflow.com/users/163699/spudly">StackOverflow</a></li>
+      <li><a rel="me" href="https://www.linkedin.com/in/stephensorensen">LinkedIn</a></li>
+      <li><a rel="me" className="u-email" href="mailto:shuoink@gmail.com">Email: shuoink@gmail.com</a></li>
+    </ul>
   </div>;
 
 Bio.displayName = 'Bio';
