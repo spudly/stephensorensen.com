e9e13a672ae88c222b97b86628a09eb4e8a9d381
Stephen John Sorensen <spudly@users.noreply.github.com>
2017-01-24 12:49:25 -0500

    rel="me"

--- src/Social.js
+++ src/Social.js
@@ -5,12 +5,13 @@ const Social = () => (
     <h2>You can find me on...</h2>
 
     <ul>
-      <li><a href="https://www.facebook.com/stephenjohnsorensen">Facebook</a></li>
-      <li><a href="https://twitter.com/shuoink">Twitter</a></li>
-      <li><a href="https://medium.com/@shuoink">Medium</a></li>
-      <li><a href="https://github.com/spudly">Github</a></li>
-      <li><a href="http://stackoverflow.com/users/163699/spudly">StackOverflow</a></li>
-      <li><a href="https://www.linkedin.com/in/stephensorensen">LinkedIn</a></li>
+      <li><a rel="me" href="https://www.facebook.com/stephenjohnsorensen">Facebook</a></li>
+      <li><a rel="me" href="https://twitter.com/shuoink">Twitter</a></li>
+      <li><a rel="me" href="https://medium.com/@shuoink">Medium</a></li>
+      <li><a rel="me" href="https://github.com/spudly">Github</a></li>
+      <li><a rel="me" href="http://stackoverflow.com/users/163699/spudly">StackOverflow</a></li>
+      <li><a rel="me" href="https://www.linkedin.com/in/stephensorensen">LinkedIn</a></li>
+      <li><a rel="me" href="mailto:shuoink@gmail.com">Email: shuoink@gmail.com</a></li>
     </ul>
 
   </div>
