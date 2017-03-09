9c3a9bac7cfc26a21402b828835f0d95bff4a7f5
Stephen Sorensen <shuoink@gmail.com>
2017-03-08 22:02:40 -0500

    upgraded dependencies

--- scripts/utils/buildCss.js
+++ scripts/utils/buildCss.js
@@ -6,15 +6,13 @@ import {SRC, BUILD} from './paths';
 
 const outputFile = pify(fs.outputFile);
 const readFile = pify(fs.readFile);
-// TODO: reenable cssnano
-// import cssnano from 'cssnano';
 
 const buildCss = async () => {
   const plugins = [postcssImport];
   const css = await readFile(`${SRC}/css/index.css`);
   const result = await postcss(plugins).process(css, {
     from: `${SRC}/css/index.css`,
-    to: `${BUILD}/css/index.css`,
+    to: `${BUILD}/css/index.css`
   });
   await outputFile(`${BUILD}/index.css`, result.css);
 };
