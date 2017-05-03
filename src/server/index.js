350fcdda8c42ba3c0979ea1f571052b8bdacb203
Stephen Sorensen <shuoink@gmail.com>
2017-05-02 22:10:17 -0400

    upgrade http-fantasy-land, simplify server config

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,4 @@
-import {listen, composeRoutes, setHeader, get, sendFile} from 'http-fantasy-land';
+import {listen, composeRoutes, setHeader, dir} from 'http-fantasy-land';
 
 const PORT = 8080;
 
@@ -6,14 +6,7 @@ const contentSecurityPolicy = setHeader('Content-Security-Policy');
 
 const server = composeRoutes(
   contentSecurityPolicy("default-src 'self'; img-src 'self' www.gravatar.com"),
-  get('/', sendFile(`${__dirname}/../client/index.html`)),
-  get('/css', sendFile(`${__dirname}/../client/index.css`)),
-  get('/icons/medium.svg', sendFile(`${__dirname}/../client/icons/medium.svg`)),
-  get('/icons/github.svg', sendFile(`${__dirname}/../client/icons/github.svg`)),
-  get('/icons/stack-overflow.svg', sendFile(`${__dirname}/../client/icons/stack-overflow.svg`)),
-  get('/icons/twitter.svg', sendFile(`${__dirname}/../client/icons/twitter.svg`)),
-  get('/icons/email.svg', sendFile(`${__dirname}/../client/icons/email.svg`)),
-  get('/icons/facebook.svg', sendFile(`${__dirname}/../client/icons/facebook.svg`)),
+  dir(`${__dirname}/../client`),
 );
 
 listen(server, {port: PORT})
