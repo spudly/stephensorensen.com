85daec060c32722aba6e04254674931f4081cd59
Stephen Sorensen <shuoink@gmail.com>
2019-03-08 23:08:50 -0500

    fixed typescript errors

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import CodeEditor from "./CodeEditor";
 import Screensaver from "./Screensaver";
 import useIsIdle from "./hooks/useIsIdle";
