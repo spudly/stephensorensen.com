90ae3e03f6ac57e9f08486dfd6a40c3171193b7e
Stephen John Sorensen <spudly@users.noreply.github.com>
2019-10-18 09:23:25 -0400

    Update CodeEditor.tsx

--- src/js/CodeEditor.tsx
+++ src/js/CodeEditor.tsx
@@ -5,11 +5,15 @@ type Props = {
 };
 
 const CodeEditor = ({ initialValue }: Props) => {
-  const [value, setValue] = useState(initialValue);
-  const handleValueChange = useCallback(e => setValue(e.currentTarget.value), [
-    setValue
-  ]);
-  return <textarea value={value} onChange={handleValueChange} />;
+  //const [value, setValue] = useState(initialValue);
+  //const handleValueChange = useCallback(e => setValue(e.currentTarget.value), [
+  //  setValue
+  //]);
+  //return <textarea value={value} onChange={handleValueChange} />;
+  return (
+    <pre style={{margin: 0, width: '100vw', height: '100vh'}}>// TODO: create website.
+// In the meantime, checkout my presentation @ Connect.Tech 2019: <a href="https://react-patterns.stephensorensen.com/">Advanced React Design Patterns</a></pre>
+  );
 };
 
 export default CodeEditor;
